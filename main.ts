af050f4168d16492f7be7da2eb38194f43436766
Felipe Lima <felipedlimacontato@gmail.com>
2023-12-29 14:45:26 -0300

    initial commit

---
+++ main.ts
@@ -0,0 +1,47 @@
+import { Notice, Plugin } from "obsidian";
+
+// Remember to rename these classes and interfaces!
+
+interface CopyImagePluginSettings {
+	mySetting: string;
+}
+
+export default class CopyImagePlugin extends Plugin {
+	settings: CopyImagePluginSettings;
+
+	async onload() {
+		await this.loadSettings();
+
+		this.registerDomEvent(document, "contextmenu", (evt: MouseEvent) => {
+			const images = document.getElementsByTagName("img");
+			(async () => {
+				for (let i = 0; i < images.length; i++) {
+					if (images[i].contains(evt.target as Node)) {
+						const response = await fetch(images[i].src);
+						const imageBlob = await response.blob();
+						navigator.clipboard
+							.write([
+								new ClipboardItem({
+									[imageBlob.type]: imageBlob,
+								}),
+							])
+							.then(() => {
+								new Notice("Copied image URL to clipboard!");
+							});
+						break;
+					}
+				}
+			})();
+		});
+	}
+
+	onunload() {}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
