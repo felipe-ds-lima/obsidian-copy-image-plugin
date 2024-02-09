7ea4d142fb723da25889d742db1b750e3cb26b90
Felipe Lima <felipedlimacontato@gmail.com>
2024-02-09 15:47:11 -0300

    add a delay to copy image

--- main.ts
+++ main.ts
@@ -19,32 +19,31 @@ export default class CopyImagePlugin extends Plugin {
 						}
 					}
 
-					const images = document.getElementsByTagName("img");
-					(async () => {
-						for (let i = 0; i < images.length; i++) {
-							if (images[i].contains(evt.target as Node)) {
-								const response = await fetch(images[i].src);
-								const imageBlob = await response.blob();
-								navigator.clipboard
-									.write([
-										new ClipboardItem({
-											[imageBlob.type]: imageBlob,
-										}),
-									])
-									.then(() => {
-										new Notice(
-											"Copied image URL to clipboard!"
-										);
-									})
-									.catch(() => {
-										new Notice(
-											"Failed to copy image URL to clipboard!"
-										);
-									});
-								break;
-							}
+					setTimeout(async () => {
+						if (
+							evt.target instanceof HTMLImageElement &&
+							evt.target.tagName === "IMG"
+						) {
+							const response = await fetch(evt.target.src);
+							const imageBlob = await response.blob();
+							navigator.clipboard
+								.write([
+									new ClipboardItem({
+										[imageBlob.type]: imageBlob,
+									}),
+								])
+								.then(() => {
+									new Notice(
+										"Copied image URL to clipboard!"
+									);
+								})
+								.catch(() => {
+									new Notice(
+										"Failed to copy image URL to clipboard!"
+									);
+								});
 						}
-					})();
+					}, 100);
 				}
 			);
 		} else {
